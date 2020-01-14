5df6ecf5827008cae602febf414ff72f97742bbc
zhanglei <zhagnlei@wowkai.cn>
2020-01-14 17:29:29 +0800

    测试外层数据返回

--- app/src/main/java/com/banketree/tt_camera_demo/MainActivity.kt
+++ app/src/main/java/com/banketree/tt_camera_demo/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.banketree.tt_camera_demo
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.ttm.camera_component.ui.CameraActivity
+import com.ttm.camera_component.ui.PreviewActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,11 +17,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         test_tv.setOnClickListener {
-            startActivity(Intent(this, TakeCameraActivity::class.java))
+            startActivityForResult(Intent(this, TakeCameraActivity::class.java), 200)
         }
         test_two.setOnClickListener {
         }
         test_three.setOnClickListener {
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 200 && data != null) {
+            val path = data.getStringExtra(PreviewActivity.PARAMS_PATH)
+            val flagPicture = data.getBooleanExtra(PreviewActivity.PARAMS_FLAG_PICTURE, true)
+            if (TextUtils.isEmpty(path) || !File(path).exists()) {
+                return
+            }
+            if (resultCode == Activity.RESULT_OK) {
+                val intent = Intent()
+                intent.putExtra(PreviewActivity.PARAMS_PATH, path)
+                intent.putExtra(PreviewActivity.PARAMS_FLAG_PICTURE, flagPicture)
+                Toast.makeText(this, path, Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
